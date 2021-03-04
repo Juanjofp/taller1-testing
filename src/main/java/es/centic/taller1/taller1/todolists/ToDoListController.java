ddc1837750a485f48d99bd6bf30ce6007d75e0a2
Juanjo Franco <juanjo@juanjofp.com>
2021-03-04 16:53:00 +0100

    test passed

--- src/main/java/es/centic/taller1/taller1/todolists/ToDoListController.java
+++ src/main/java/es/centic/taller1/taller1/todolists/ToDoListController.java
@@ -1,8 +1,15 @@
 package es.centic.taller1.taller1.todolists;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
 
 @RestController
 public class ToDoListController {
     
+    @GetMapping(value="/lists", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String showAllLists() {
+        return "[]";
+    }
 }
