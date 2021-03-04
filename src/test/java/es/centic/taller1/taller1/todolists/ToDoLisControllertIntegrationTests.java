00a9d1d4a00cad8c1636b584d4ce8258c2a9a830
Juanjo Franco <juanjo@juanjofp.com>
2021-03-04 16:48:31 +0100

    test

--- src/test/java/es/centic/taller1/taller1/todolists/ToDoLisControllertIntegrationTests.java
+++ src/test/java/es/centic/taller1/taller1/todolists/ToDoLisControllertIntegrationTests.java
@@ -2,18 +2,37 @@ package es.centic.taller1.taller1.todolists;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.assertj.core.api.Assertions.*;
 
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ToDoLisControllertIntegrationTests {
     @Autowired
     private ToDoListController toDoListController;
 
+    @Autowired
+    private MockMvc mockMvc;
+
     @Test
     void checkToDoListController() {
         assertThat(toDoListController).isNotNull();
+    }
+
+    @Test
+    void checkToDoListControllerIsItsPath() throws Exception {
+        // Arrange
+        MockHttpServletRequestBuilder request = MockMvcRequestBuilders.get("/lists");
+
+        // Act
+        mockMvc.perform(request)
+            .andExpect(MockMvcResultMatchers.status().isOk());
     }
 }
